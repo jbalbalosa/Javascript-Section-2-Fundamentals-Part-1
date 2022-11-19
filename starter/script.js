3605047798dff22914b19237bebd89533adb7656
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-19 20:28:41 +0900

    update

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is fun!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jeff');
+console.log('Centennial 2024')
+
+let firstName = "Jeff Balbalosa";
+console.log(firstName);
